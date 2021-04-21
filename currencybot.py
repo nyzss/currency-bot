d3028bacb9a75dbc88aaded0d92cd8bbf0e65855
nyzs <neazenmeh@gmail.com>
2021-04-21 18:41:59 +0300

    push

--- currencybot.py
+++ currencybot.py
@@ -2,9 +2,6 @@ import requests
 import discord
 from discord.ext import commands
 import json
-#import forex_python
-#from forex_python.converter import CurrencyRates
-#from forex_python.converter import CurrencyCodes
 from PIL import Image, ImageFont, ImageDraw
 import PIL
 from urllib.request import urlopen
@@ -135,6 +132,7 @@ async def list(ctx):
     await ctx.send(embed=list_embed)
 
 
+@client.event
 @client.command(brief="WORKING ONE")
 async def testing_convert(ctx, first_currency: str, second_currency: str, value: float):
 
@@ -152,32 +150,6 @@ async def testing_convert(ctx, first_currency: str, second_currency: str, value:
     second_symbol = symbol.get_symbol(second_currency)
 
     await ctx.send(f"{value}{first_symbol} to {second_symbol} is equal to {currency_converted}{second_symbol}")
-
- # do the float thingy yaknow .2f and then add the image manipulator with pillow
-
-
-# YOU GET ERRORS HERE
-""" @client.command(brief='Convert a currency to another. Converting 5 euro to usd would look like this $eur usd 5', aliases=['c', 'conversion', 'conv', 'C', 'CONV', 'conv', 'CONVERSION', 'CONVERT'])
-async def converter(ctx, first_currency: str, second_currency: str, value: float):
-
-    # here you want to .upper them for it to be acceptable?
-    first_currency.upper()
-    second_currency.upper()
-
-    # here you want to convert the input currencies and values.
-    currency_conversion = converter.convert(
-        first_currency, second_currency, value)
- """
-
-# ICI CODE TOUT DE BASE
-
-
-# EN BAS N'EST PAS IMPORTANT POUR L'INSTANT
-# EN BAS N'EST PAS IMPORTANT POUR L'INSTANT
-# EN BAS N'EST PAS IMPORTANT POUR L'INSTANT
-# EN BAS N'EST PAS IMPORTANT POUR L'INSTANT
-# EN BAS N'EST PAS IMPORTANT POUR L'INSTANT
-# EN BAS N'EST PAS IMPORTANT POUR L'INSTANT
 
 
 @client.command()
@@ -197,71 +169,6 @@ async def test(ctx, *, text):
     file = discord.File("image.png", filename="image.png")
     embed.set_image(url="attachment://image.png")
     await ctx.send(file=file, embed=embed)
-
-
-""" @client.command(aliases=['c', 'conv', 'C', 'CONV', 'CON', 'con', 'conversion'], brief="A converter built with the python forex framework, meant for learning")
-async def convert(ctx, currency_name: str, currency_value: float):
-
-    #random_message_number = random.randint(0, len(random_message) - 1)
-    currency_name.upper()
-
-    currency_converted = converter.convert(
-        currency_name, 'TRY', currency_value)
-
-    currency_symbol = codes.get_symbol(currency_name)
-
-    currency_image = Image.open("currency_bg.png")
-    font = ImageFont.truetype("RobotoMono-Medium.ttf", 36)
-
-    draw = ImageDraw.Draw(currency_image)
-
-    text.upper()
-
-    text = f"{currency_value}{currency_symbol} = {currency_converted:.2f}"
-
-    draw.text((160, 40), text, (0, 0, 0), font=font)
-    currency_image.save("image.png")
-
-    currency_embed = discord.Embed(
-        title="annen", color=0x4200BD)  # creates embed
-    file = discord.File("image.png", filename="image.png")
-    currency_embed.set_image(url="attachment://image.png")
-    # await ctx.send(file=file, embed=embed)
-
-    await ctx.send(embed=currency_embed) """
-
-
-""" currency_converter_embed = discord.Embed(
-        title=f"Converting: {currency_value}-{currency_symbol} is equal to {currency_converted:.2f}₺",
-        #title=f"Converting: {currency_value} of {currency_symbol} ({currency_name}) is equal to {currency_converted:.2f}₺ (TRY)",
-        #description=f"Converting {currency_value} of {currency_symbol} ({currency_name}) to ₺ (TRY), {random_message[random_message_number]}",
-        color=0x4200BD
-    ) """
-
-
-""" @client.command(brief='Convert a currency to another. Converting 5 euro to usd would look like this $eur usd 5', aliases=['c', 'conversion', 'conv', 'C', 'CONV', 'conv', 'CONVERSION', 'CONVERT'])
-async def convert(ctx, first_currency: str, second_currency: str, value: float):
-
-    first_currency.upper()
-    second_currency.upper()
-
-    currency_converted = converter.convert()
-
-    currency_image = Image.open("currency_bg.png")
-    font = ImageFont.truetype("RobotoMono-Medium.ttf", 36)
-
-    draw = ImageDraw.Draw(currency_image)
-
-    draw.text((160, 40), text, (0, 0, 0), font=font)
-    currency_image.save("image.png")
-
-    text.upper()
-
-    embed = discord.Embed(title="Title", description="Desc",
-                          color=0x00ff00)  # creates embed
-    file = discord.File("image.png", filename="image.png")
-    embed.set_image(url="attachment://image.png")
-    await ctx.send(file=file, embed=embed) """
 
 
 client.run(TOKEN)
